4ae87f86a0696ccce714e0fca0acb71f6ee77356
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-03-05 09:09:55 +0100

    feat(mason): update header colors

--- lua/koda/groups/mason.lua
+++ lua/koda/groups/mason.lua
@@ -3,16 +3,17 @@ local M = {}
 local utils = require("koda.utils")
 
 ---@type koda.HighlightsFn
-function M.get_hl(c, opts)
+function M.get_hl(c)
   -- stylua: ignore
   return {
-    MasonNormal              = { bg = opts.transparent and "none" or c.bg },
-    MasonHeader              = { fg = c.highlight, bg = utils.blend(c.highlight, c.bg, 0.2), bold = true },
-    MasonHeaderSecondary     = { fg = c.const, bg = utils.blend(c.const, c.bg, 0.2), bold = true },
-    MasonHighlight           = { fg = c.const },
-    MasonHighlightBlock      = { fg = c.success, bg = utils.blend(c.green, c.bg, 0.2) },
-    MasonHighlightBlockBold  = { fg = c.dim, bg = c.emphasis, bold = true },
-    MasonMutedBlock          = { fg = c.dim, bg = c.keyword },
+    MasonNormal                      = { link = "Normal" },
+    MasonHeader                      = { fg = c.highlight, bg = utils.blend(c.highlight, c.bg, 0.2), bold = true },
+    MasonHeaderSecondary             = { fg = c.const, bg = utils.blend(c.const, c.bg, 0.2), bold = true },
+    MasonHighlight                   = { fg = c.const },
+    MasonHighlightBlock              = { fg = c.success, bg = utils.blend(c.green, c.bg, 0.2) },
+    MasonHighlightBlockBold          = { fg = c.highlight, bg = utils.blend(c.highlight, c.bg, 0.2) },
+    MasonHighlightBlockBoldSecondary = { fg = c.const, bg = utils.blend(c.const, c.bg, 0.2) },
+    MasonMutedBlock                  = { fg = c.fg, bg = c.line },
   }
 end
 
